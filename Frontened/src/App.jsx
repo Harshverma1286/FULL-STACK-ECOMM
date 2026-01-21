7df45bb15db6c706f8c9a28a31c289b87133fd48
Harsh Verma <hv0626266@gmail.com>
2026-01-21 15:54:54 +0530

    ADDED ALL THE FILES

---
+++ Frontened/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { Route } from 'react-router-dom'
+import {Home,Collection,About, Contact, Product, Cart, Login, PlaceOrder, Order} from './Pages/Allpage'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/order' element={<Order/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
