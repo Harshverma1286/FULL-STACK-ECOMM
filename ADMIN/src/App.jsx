409cb4360ad68cd1bf0992a9f51f5df83deb955d
Harsh Verma <hv0626266@gmail.com>
2026-02-05 01:09:17 +0530

    BUILDING THE ADMIN PART !!

--- ADMIN/src/App.jsx
+++ ADMIN/src/App.jsx
@@ -1,14 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './Components/Navbar'
 import './App.css'
+import Sidebar from './Components/Sidebar'
+import {Routes,Route} from 'react-router-dom';
+import Add from '../src/Pages/Add';
+import List from '../src/Pages/List';
+import Order from '../src/Pages/Order';
+import Login from './Components/Login';
+
+ import { ToastContainer, toast } from 'react-toastify';
+
+
+export const backendurl = import.meta.env.VITE_BACKEND_URL;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token,settoken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
+
+
+  useEffect(()=>{
+      localStorage.setItem('token',token);
+  },[token]);
+
+
 
   return (
-    <>
-    </>
+    <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer/>
+      {token ==="" ? <Login settoken={settoken}/> : 
+      <>
+      <Navbar settoken={settoken}/>
+      <hr/>
+      <div className='flex w-full'>
+        <Sidebar/>
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+          <Routes>
+            <Route path='/add' element={<Add token={token}/>}/>
+            <Route path='/List' element={<List token={token}/>}/>
+            <Route path='/order' element={<Order token={token}/>}/>
+          </Routes>
+        </div>
+      </div>
+      </>}
+    </div>
   )
 }
 
--- ADMIN/src/Components/Navbar.jsx
+++ ADMIN/src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
-function Navbar() {
+function Navbar({settoken}) {
   return (
-    <div>
-      
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img className='w-[max(10%,80px)]' src={assets.LOGOS} alt="" />
+      <button onClick={()=> settoken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Log out</button>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
