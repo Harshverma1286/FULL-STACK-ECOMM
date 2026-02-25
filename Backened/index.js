0f92a6a41d028584715c6240606d53cd7494bf15
Harsh Verma <hv0626266@gmail.com>
2026-02-26 01:32:21 +0530

    fixed errors

--- Backened/index.js
+++ Backened/index.js
@@ -1,28 +1,10 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import app from '../Backened/app.js';
+import app from "./app.js";
+import connectdb from "./DB/index.js";
+import connectcloudinary from "./DB/cloudinary.js";
 
+await connectdb();
+await connectcloudinary();
 
-import connectdb from './DB/index.js';
-
-import connectcloudinary from '../Backened/DB/cloudinary.js'
-
-
-connectdb().then(
-()=>{
-    app.on('error',(error)=>{
-        console.log("err",error);
-        throw error;
-    });
-
-    app.listen(process.env.PORT || 3000,function(){
-        console.log(`THE APP IS SUCCESSFULLY WORKING ON PORT ${process.env.PORT}`)
-    })
-}
-).catch(
-    (err)=>{
-        console.log("err",err);
-    }
-)
-
-connectcloudinary();
+export default app;
